22d6d07413d2c5abbaa7544834b6680d426d1d03
N00byEdge <hannesbredberg@gmail.com>
2020-02-13 22:35:23 +0100

    KernelLoader: Load the kernel!

--- KernelLoader/KernelLoader.cpp
+++ KernelLoader/KernelLoader.cpp
@@ -22,75 +22,178 @@ extern "C" u64 displayHeight;
 extern "C" u64 displayPitch;
 extern "C" u64 framebuffer;
 extern "C" u64 driveNumber;
+extern "C" u8 bundledKernel[];
+extern "C" u8 bundledKernelEnd[];
 
 namespace {
   constexpr bool quiet = false;
   auto pline = flo::makePline<quiet>("[FLORKLOAD] ");
+
+  flo::ELF64Image kernelELF{bundledKernel, bundledKernelEnd - bundledKernel};
 }
 
-namespace flo {
-  void putchar(char c) {
-    if constexpr(!quiet)
-      flo::IO::serial1.write(c);
+extern "C" {
+  u64 kernelEntry;
+
+  flo::KernelArguments kernelArguments {
+    &kernelELF
+  };
+}
+
+void flo::putchar(char c) {
+  if constexpr(!quiet)
+    flo::IO::serial1.write(c);
+}
+
+void flo::feedLine() {
+  if constexpr(!quiet)
+    flo::IO::serial1.write('\n');
+}
+
+void flo::setColor(flo::IO::Color col) {
+  if constexpr(!quiet)
+    flo::IO::serial1.setColor(col);
+}
+
+u8 *flo::getPtrPhys(flo::PhysicalAddress phys) {
+  return (u8 *)(phys() + physBase());
+}
+
+u8 *flo::getPtrVirt(flo::VirtualAddress virt) {
+  return (u8 *)virt();
+}
+
+void flo::returnPhysicalPage(flo::PhysicalAddress phys, int pageLevel) {
+  switch(pageLevel) {
+    case 1: *getPhys<PhysicalAddress>(phys) = flo::exchange(physFreeHead1, phys); return;
+    case 2: *getPhys<PhysicalAddress>(phys) = flo::exchange(physFreeHead2, phys); return;
+    case 3: *getPhys<PhysicalAddress>(phys) = flo::exchange(physFreeHead3, phys); return;
+    case 4: *getPhys<PhysicalAddress>(phys) = flo::exchange(physFreeHead4, phys); return;
+    case 5: *getPhys<PhysicalAddress>(phys) = flo::exchange(physFreeHead5, phys); return;
+    default: pline("Unkown paging level: ", Decimal{pageLevel}); flo::CPU::hang();
+  }
+}
+
+flo::PhysicalAddress flo::getPhysicalPage(int pageLevel) {
+  auto tryGet =
+    [pageLevel](flo::PhysicalAddress &currHead) {
+      // Fast path, try to get from current level
+      if(currHead())
+        return flo::exchange(currHead, *getPhys<PhysicalAddress>(currHead));
+
+      if(pageLevel == 5)
+        return PhysicalAddress{0};
+
+      // Slow path, try to get from next level
+      auto next = flo::getPhysicalPage(pageLevel + 1);
+
+      if(!next) {
+        if(pageLevel == 1) {
+          pline("Ran out of physical pages on level ", pageLevel);
+          flo::CPU::hang();
+        }
+
+        if(!next)
+          return PhysicalAddress{0};
+      }
+
+      // Woop we got one, let's split it.
+      auto stepSize = flo::Paging::pageSizes[pageLevel - 1]; // 0 indexed, we are 1 indexed
+
+      // Return all pages but one
+      for(int i = 0; i < flo::Paging::PageTableSize - 1; ++ i) {
+        flo::returnPhysicalPage(next, pageLevel);
+        next += PhysicalAddress{stepSize};
+      }
+
+      return next;
+    };
+
+  switch(pageLevel) {
+    case 1: return tryGet(physFreeHead1);
+    case 2: return tryGet(physFreeHead2);
+    case 3: return tryGet(physFreeHead3);
+    case 4: return tryGet(physFreeHead4);
+    case 5: return tryGet(physFreeHead5);
+    default: pline("Unknown page level ", pageLevel); flo::CPU::hang();
   }
 
-  void feedLine() {
-    if constexpr(!quiet)
-      flo::IO::serial1.write('\n');
-  }
-
-  void setColor(flo::IO::Color col) {
-    if constexpr(!quiet)
-      flo::IO::serial1.setColor(col);
-  }
-
-  u8 *getPtrPhys(flo::PhysicalAddress phys) {
-    return (u8 *)(phys() + physBase());
-  }
-
-  void returnPhysicalPage(flo::PhysicalAddress phys, int pageLevel) {
-    switch(pageLevel) {
-      case 1: *getPhys<PhysicalAddress>(phys) = std::exchange(physFreeHead1, phys); return;
-      case 2: *getPhys<PhysicalAddress>(phys) = std::exchange(physFreeHead2, phys); return;
-      case 3: *getPhys<PhysicalAddress>(phys) = std::exchange(physFreeHead3, phys); return;
-      case 4: *getPhys<PhysicalAddress>(phys) = std::exchange(physFreeHead4, phys); return;
-      case 5: *getPhys<PhysicalAddress>(phys) = std::exchange(physFreeHead5, phys); return;
-      default: pline("Unkown paging level: ", Decimal{pageLevel}); flo::CPU::hang();
-    }
-  }
+  __builtin_unreachable();
+  return PhysicalAddress{0};
 }
 
 extern "C" void assertAssumptions() {
   auto check =
-    [](auto &value) {
-      auto v = *(u64*)&value;
+    [](u64 *value) {
+      auto v = *value;
       if(unknownField == v) {
         pline("Unset field ", v, "!!");
         flo::CPU::hang();
       }
     };
 
-  check(physFreeHead1);
-  check(physFreeHead2);
-  check(physFreeHead3);
-  check(physFreeHead4);
-  check(physFreeHead5);
-  check(physMemRanges);
-  check(displayWidth);
-  check(displayHeight);
-  check(displayPitch);
-  check(framebuffer);
-  check(driveNumber);
+  check((u64*)&physFreeHead1);
+  check((u64*)&physFreeHead2);
+  check((u64*)&physFreeHead3);
+  check((u64*)&physFreeHead4);
+  check((u64*)&physFreeHead5);
+  check((u64*)&physMemRanges);
+  check((u64*)&displayWidth);
+  check((u64*)&displayHeight);
+  check((u64*)&displayPitch);
+  check((u64*)&framebuffer);
+  check((u64*)&driveNumber);
 }
 
 extern "C" void consumeHighPhysicalMemory() {
-  for(auto &r: *physMemRanges)
+  for(auto &r: *physMemRanges) {
+    pline("Consuming physical memory [", r.begin(), ", ", r.end(), ")");
     flo::consumePhysicalMemory(r.begin, r.end() - r.begin());
+  }
 }
 
 extern "C" void unmapLowMemory() {
-  constexpr auto lowMemoryLimit = flo::Util::mega(512ull);
+  // Don't return the identity mapped pages
+  flo::Paging::unmap<false>(flo::VirtualAddress{0}, flo::Util::mega(512ull));
+}
 
-  // Don't return the identity mapped pages, that sounds bad...
-  flo::Paging::unmap<false>(flo::VirtualAddress{0}, lowMemoryLimit);
+extern "C" void loadKernel() {
+  auto loadFail = [](auto &&...vs) {
+    pline("Error while loading kernel ELF: ", flo::forward<decltype(vs)>(vs)...);
+    flo::CPU::halt();
+  };
+
+  kernelELF.verify(flo::move(loadFail));
+
+  u64 addrHigh = 0;
+
+  kernelELF.forEachProgramHeader([&](flo::ELF64::ProgramHeader const &header) {
+    u64 sectionAddrHigh = flo::Paging::alignPageUp(header.vaddr() + header.memSz);
+    if(sectionAddrHigh > addrHigh)
+      addrHigh = sectionAddrHigh;
+  });
+
+  addrHigh = flo::Paging::alignPageUp<1>(addrHigh);
+
+  kernelELF.loadOffset = (physBase - flo::VirtualAddress{addrHigh})();
+  pline("Kernel needs to be offset by ", kernelELF.loadOffset);
+
+  kernelELF.loadAll();
+
+  kernelEntry = kernelELF.header().entry() + kernelELF.loadOffset;
+  pline("Entering kernel...");
+}
+
+extern "C" void prepareKernelArgs() {
+  kernelArguments.physFreeHead1 = physFreeHead1;
+  kernelArguments.physFreeHead2 = physFreeHead2;
+  kernelArguments.physFreeHead3 = physFreeHead3;
+  kernelArguments.physFreeHead4 = physFreeHead4;
+  kernelArguments.physFreeHead5 = physFreeHead5;
+  kernelArguments.physBase = physBase;
+  kernelArguments.displayWidth = displayWidth;
+  kernelArguments.displayHeight = displayHeight;
+  kernelArguments.displayPitch = displayPitch;
+  kernelArguments.framebuffer = framebuffer;
+  kernelArguments.driveNumber = driveNumber;
 }
---
+++ include/flo/ELF.hpp
@@ -0,0 +1,371 @@
+#pragma once
+
+#include "flo/Florence.hpp"
+#include "flo/TypeTraits.hpp"
+#include "flo/Bitfields.hpp"
+#include "flo/IO.hpp"
+#include "flo/Algorithm.hpp"
+#include "flo/Paging.hpp"
+
+namespace flo {
+  namespace ELF {
+    enum struct ObjectClass: u8 {
+      ELF32 = 1,
+      ELF64 = 2,
+    };
+
+    enum struct DataEncoding: u8 {
+      LittleEndian = 1,
+      BigEndian    = 2,
+    };
+
+    enum struct OSABI: u8 {
+      SystemV    = 0,
+      HPUX       = 1,
+      Standalone = 255,
+    };
+
+    enum struct Version: u8 {
+      None    = 0,
+      Current = 1,
+    };
+
+    enum struct ObjectType: u16 {
+      None         = 0,
+      Relocatable  = 1,
+      Executable   = 2,
+      SharedObject = 3,
+      Core         = 4,
+    };
+  }
+
+  namespace ELF32 {
+    struct addr: flo::StrongTypedef<addr, u32> {
+      using flo::StrongTypedef<addr, u32>::StrongTypedef;
+    };
+
+    struct foff: flo::StrongTypedef<foff, u32> {
+      using flo::StrongTypedef<foff, u32>::StrongTypedef;
+    };
+
+    struct Header {
+      u8                magic[0x4];
+      ELF::ObjectClass  fileclass;
+      ELF::DataEncoding encoding;
+      ELF::Version      fileversion;
+      u8                padding[9];
+      ELF::ObjectType   type;
+      u16               machine;
+      ELF::Version      version;
+      addr              entry;
+      foff              phoff;
+      foff              shoff;
+      u16               flags;
+      u16               ehsize;
+      u16               phentsize;
+      u16               phnum;
+      u16               shentsize;
+      u16               shnum;
+      u16               shstrndx;
+    };
+  }
+
+  namespace ELF64 {
+    struct addr: flo::StrongTypedef<addr, u64> {
+      using flo::StrongTypedef<addr, u64>::StrongTypedef;
+    };
+
+    struct foff: flo::StrongTypedef<foff, u64> {
+      using flo::StrongTypedef<foff, u64>::StrongTypedef;
+    };
+
+    struct Header {
+      u8                magic[0x4];
+      ELF::ObjectClass  fileclass;
+      ELF::DataEncoding encoding;
+      ELF::Version      fileversion;
+      ELF::OSABI        osabi;
+      u8                abiversion;
+      u8                padding[7];
+      ELF::ObjectType   type;
+      u16               machine;
+      ELF::Version      version;
+      addr              entry;
+      foff              phoff;
+      foff              shoff;
+      u32               flags;
+      u16               ehsize;
+      u16               phentsize;
+      u16               phnum;
+      u16               shentsize;
+      u16               shnum;
+      u16               shstrndx;
+    };
+
+    struct ProgramHeader {
+      enum struct Type: u32 {
+        Null    = 0,
+        Load    = 1,
+        Dynamic = 2,
+        Interp  = 3,
+        Note    = 4,
+        Shlib   = 5,
+        Phdr    = 6,
+      };
+
+      enum Flags: u32 {
+        Executable = 1 << 0,
+        Writeable  = 1 << 1,
+        Readable   = 1 << 2,
+      };
+
+      Type  type;
+      Flags flags;
+      foff  offset;
+      addr  vaddr;
+      addr  paddr;
+      u64   fileSz;
+      u64   memSz;
+      u64   align;
+    };
+
+    struct SectionHeader {
+      enum struct Type: u32 {
+        null     = 0,
+        progbits = 1,
+        symtab   = 2,
+        strtab   = 3,
+        rela     = 4,
+        hash     = 5,
+        dynamic  = 6,
+        note     = 7,
+        nobits   = 8,
+        rel      = 9,
+        dynsym   = 11,
+      };
+
+      enum Flags: u32 {
+        Write = 1 << 0,
+        Alloc = 1 << 1,
+        Code  = 1 << 2,
+      };
+
+      u32   name;
+      Type  type;
+      Flags flags;
+      addr  baseAddr;
+      foff  offset;
+      u64   size;
+      u32   link;
+      u32   info;
+      u64   alignment;
+      u64   entsize;
+    };
+
+    struct RelocationEntry {
+      enum RelocType {
+        X86_64_RELATIVE = 8,
+      };
+
+      addr address;
+      union {
+        u64 info;
+        Bitfield<0, 32> type;
+        Bitfield<32, 32> symbol;
+      };
+
+      i64 addend;
+
+      u64 size() const { return 8; }
+      void apply(uSz loadOffset) const {
+        auto at = (u64 *)(address() + loadOffset);
+        switch(type) {
+          break; case RelocType::X86_64_RELATIVE:
+            *(at) = loadOffset + addend;
+          break; default: break;
+        }
+      }
+
+      bool valid() const {
+        switch(type) {
+          case RelocType::X86_64_RELATIVE: return true;
+          default: return false;
+        }
+      }
+    };
+  }
+
+  struct ELF64Image {
+    u8 const *data;
+    iSz size;
+
+    // Assumed always 4K aligned
+    uSz loadOffset = 0;
+
+    template<typename Fail>
+    void verify(Fail &&fail) {
+      verify_inside_file(ELF64::foff{0}, sizeof(ELF64::Header), move(fail));
+
+      if(!equals(header().magic, "\x7F""ELF"))
+        return forward<Fail>(fail)("Invalid magic for ELF file: ",
+          (u8)header().magic[0], " ", (u8)header().magic[1], " ", (u8)header().magic[2], " ", (u8)header().magic[3]);
+
+      if(header().fileclass != ELF::ObjectClass::ELF64)
+        return forward<Fail>(fail)("Unexpected class: ", flo::Decimal{static_cast<u8>(header().fileclass)});
+
+      if(header().version != ELF::Version::Current)
+        return forward<Fail>(fail)("Unexpected version: ", flo::Decimal{static_cast<u8>(header().version)});
+
+      if(!entry())
+        return forward<Fail>(fail)("No entry point found: ", header().entry());
+
+      if(header().phentsize < sizeof(ELF64::ProgramHeader))
+        return forward<Fail>(fail)("Program header size (", header().shentsize, ") too low, expected at least ", sizeof(ELF64::ProgramHeader));
+
+      if(header().phnum < 1)
+        return forward<Fail>(fail)("Expecting at least one program header!");
+
+      verify_inside_file(header().phoff, header().phentsize * header().phnum, move(fail));
+
+      if(header().shentsize < sizeof(ELF64::SectionHeader))
+        return forward<Fail>(fail)("Section header size (", header().shentsize, ") too low, expected at least ", sizeof(ELF64::SectionHeader));
+
+      if(header().shnum < 1)
+        return forward<Fail>(fail)("Expecting at least one section header!");
+
+      verify_inside_file(header().shoff, header().shentsize * header().shnum, move(fail));
+
+      forEachSection([&](ELF64::SectionHeader const &header) {
+        // Nobits are zero initialized and don't have backing bytes in the image
+        if(header.type != ELF64::SectionHeader::Type::nobits)
+          verify_inside_file(header.offset, header.size, move(fail));
+
+        if(header.type == ELF64::SectionHeader::Type::rela)
+          // There are relocations in this section, let's take a quick look at them.
+          forEachRelocation(header, [&](ELF64::RelocationEntry const &relent) {
+            verify_inside_loaded(relent.address, relent.size(), move(fail));
+            if(!relent.valid())
+              return forward<Fail>(fail)("Invalid relocation type ", (u32)relent.type);
+          });
+
+        if(header.type == ELF64::SectionHeader::Type::rel)
+          return forward<Fail>(fail)("REL section handling not implemented yet.");
+      });
+
+      forEachProgramHeader([&](ELF64::ProgramHeader const &header) {
+        verify_inside_file(header.offset, header.fileSz, move(fail));
+        if(header.memSz < header.fileSz)
+          forward<Fail>(fail)("memSz < fileSz!!");
+      });
+    }
+
+    ELF64::Header const &header() const {
+      return *reinterpret_cast<ELF64::Header const *>(data);
+    }
+
+    ELF64::SectionHeader const &sectionHeader(u64 ind) const {
+      return *reinterpret_cast<ELF64::SectionHeader const *>(data + header().shoff() + header().shentsize * ind);
+    }
+
+    ELF64::ProgramHeader const &programHeader(u64 ind) const {
+      return *reinterpret_cast<ELF64::ProgramHeader const *>(data + header().phoff() + header().phentsize * ind);
+    }
+
+    u8 const *fileData(ELF64::ProgramHeader const &header) const {
+      return data + header.offset();
+    }
+
+    template<typename F>
+    void forEachSection(F &&f) const {
+      for(uSz i = 1; i < header().shnum; ++ i)
+        f(sectionHeader(i));
+    }
+
+    template<typename F>
+    void forEachProgramHeader(F &&f) const {
+      for(uSz i = 0; i < header().phnum; ++ i) {
+        auto &header = programHeader(i);
+        if(header.type != ELF64::ProgramHeader::Type::Null && header.memSz)
+          f(header);
+      }
+    }
+
+    template<typename F>
+    void forEachRelocation(ELF64::SectionHeader const &shead, F &&f) const {
+      for(uSz off = 0; off < shead.size; off += sizeof(ELF64::RelocationEntry))
+        f(*reinterpret_cast<ELF64::RelocationEntry const *>(data + shead.offset() + off));
+    }
+
+    void applyAllRelocations() const {
+      forEachSection([&](ELF64::SectionHeader const &header) {
+        switch(header.type) {
+          break; case ELF64::SectionHeader::Type::rela:
+            forEachRelocation(header, [&](ELF64::RelocationEntry const &ent) {
+              ent.apply(loadOffset);
+            });
+          break; case ELF64::SectionHeader::Type::rel:
+            // Not implemented!
+          default: return;
+        }
+      });
+    }
+
+    void loadAll() const {
+      forEachProgramHeader([&](flo::ELF64::ProgramHeader const &header) {
+        if(header.type != flo::ELF64::ProgramHeader::Type::Load)
+          return;
+
+        auto sectionBase = flo::VirtualAddress{loadOffset + header.vaddr()};
+        auto sectionMemSize = flo::Paging::alignPageUp(header.memSz);
+        flo::Paging::Permissions perms;
+        perms.writeEnable = static_cast<bool>(header.flags & header.Flags::Writeable);
+        perms.mapping.executeDisable = !static_cast<bool>(header.flags & header.Flags::Executable);
+        perms.mapping.global = 0;
+        perms.allowUserAccess = 0;
+        perms.writethrough = 0;
+        perms.disableCache = 0;
+
+        auto err = flo::Paging::map(sectionBase, sectionMemSize, perms);
+        flo::checkMappingError(err, [](auto &&...) {}, flo::CPU::halt);
+
+        if(header.fileSz)
+          flo::Util::copymem((u8 *)sectionBase(), (u8 const *)fileData(header), header.fileSz);
+        if(auto set = sectionMemSize - header.fileSz; set)
+          flo::Util::setmem((u8 *)sectionBase() + header.fileSz, 0, set);
+      });
+
+      applyAllRelocations();
+    }
+
+  private:
+    template<typename Fail>
+    void verify_inside_file(ELF64::foff off, uSz region_size, Fail &&fail) {
+      if(off() + region_size <= size)
+        return;
+
+      forward<Fail>(fail)("Offset ", off(), " + size ", region_size, " = ", off() + region_size, ", not <= file size (", size, ").");
+    }
+
+    template<typename Fail>
+    void verify_inside_loaded(ELF64::addr off, uSz region_size, Fail &&fail) {
+      bool valid = false;
+      forEachProgramHeader([&](ELF64::ProgramHeader const &ph) {
+        // Begins before
+        if(off() < ph.vaddr())
+          return;
+
+        // Ends after
+        if(off() + region_size >= ph.vaddr() + ph.memSz)
+          return;
+
+        valid = true;
+      });
+      if(!valid)
+        forward<Fail>(fail)("Addr ", off(), " with size ", region_size, " not inside any LOADs");
+    }
+
+    ELF64::addr entry() {
+      return ELF64::addr{loadOffset + header().entry()};
+    }
+  };
+}
