ca76e79f5581ba5e402968fa60801c2a7c976eba
N00byEdge <hannesbredberg@gmail.com>
2020-02-19 18:08:51 +0100

    Add a small and simple panic()

--- Kernel/Kernel.cpp
+++ Kernel/Kernel.cpp
@@ -54,10 +54,33 @@ u8 *flo::getPtrVirt(flo::VirtualAddress virt) {
 }
 
 
+void panic(char const *reason) {
+  pline(flo::IO::Color::red, "Kernel panic! Reason: ", flo::IO::Color::red, reason);
+
+  auto frame = flo::getStackFrame();
+
+  pline("Backtrace: ");
+  flo::getStackTrace(frame, [](auto &stackFrame) {
+    auto symbol = arguments.elfImage->lookupSymbol(stackFrame.retaddr);
+    auto symbolName = symbol ? arguments.elfImage->symbolName(*symbol) : nullptr;
+    pline(symbolName ?: "[NO NAME]", ": ", stackFrame.retaddr - arguments.elfImage->loadOffset);
+  });
+
+  flo::CPU::halt();
+}
+
+namespace Fun::things {
+  void foo() {
+    panic("Failed successfully");
+  }
+}
+
 extern "C"
 void kernelMain() {
   pline("Hello ELF kernel land");
   pline("My ELF is loaded at ", arguments.elfImage->data, " with size ", arguments.elfImage->size);
   pline("Physical base is at ", (void *)arguments.physBase());
   pline("  Best regards, 0x", (void *)&kernelMain);
+
+  Fun::things::foo();
 }
