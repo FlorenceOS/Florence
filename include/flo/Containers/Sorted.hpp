04e9d12a607892063e8d786a7af0cf1d31fb28cf
N00byEdge <hannesbredberg@gmail.com>
2020-02-25 10:27:38 +0100

    Sorted: flo::-ify

--- include/flo/Containers/Sorted.hpp
+++ include/flo/Containers/Sorted.hpp
@@ -7,60 +7,77 @@
 #include "flo/Util.hpp"
 
 namespace flo {
-  template<typename Storage, typename Compare = std::less<>>
+  template<typename Storage, typename Compare = flo::Less<>>
   struct Sorted: Storage, Compare {
-      Sorted(Compare cmp = Compare{}): Compare{cmp} { }
+    constexpr Sorted(Compare cmp = Compare{}) : Compare{cmp} { }
 
-      template<typename Ty>
-      auto lowerBound(Ty const &val) const {
-        return std::lower_bound(this->begin(), this->end(), val, static_cast<Compare const &>(*this));
-      }
+    template<typename Ty>
+    constexpr auto lowerBound(Ty const &val) const {
+      return flo::lowerBound(container(), val, static_cast<Compare const &>(*this));
+    }
 
-      template<typename Ty>
-      auto upperBound(Ty const &val) const {
-        return std::upper_bound(this->begin(), this->end(), val, static_cast<Compare const &>(*this));
-      }
+    template<typename Ty>
+    constexpr auto lowerBound(Ty const &val) {
+      return flo::lowerBound(container(), val, static_cast<Compare const &>(*this));
+    }
 
-      template<typename Ty>
-      auto equalRange(Ty const &val) const {
-        return std::equal_range(this->begin(), this->end(), val, static_cast<Compare const &>(*this));
-      }
+    template<typename Ty>
+    constexpr auto upperBound(Ty const &val) const {
+      return flo::upperBound(container(), val, static_cast<Compare const &>(*this));
+    }
 
-      template<typename Beg, typename End>
-      auto merge(Beg beg, End end) {
-        auto num = std::distance(beg, end);
-        static_cast<Storage &>(*this).insert(this->end(), beg, end);
-        std::inplace_merge(this->begin(), this->end() - num, this->end());
-      }
+    template<typename Ty>
+    constexpr auto upperBound(Ty const &val) {
+      return flo::upperBound(container(), val, static_cast<Compare const &>(*this));
+    }
 
-      template<typename ...Ts>
-      auto emplace(Ts &&...vs) {
-        this->emplace_back(std::forward<Ts>(vs)...);
-        std::inplace_merge(this->begin(), this->end() - 1, this->end());
-      }
+    template<typename Ty>
+    constexpr auto equalRange(Ty const &val) const {
+      return flo::equalRange(container(), val, static_cast<Compare const &>(*this));
+    }
 
-      template<typename Ty>
-      auto insert(Ty const &v) {
-        return emplace(v);
-      }
+    template<typename Ty>
+    constexpr auto equalRange(Ty const &val) {
+      return flo::equalRange(container(), val, static_cast<Compare const &>(*this));
+    }
 
-      template<typename Ty>
-      auto find(Ty const &v) const {
-        auto lb = this->lowerBound(v);
-        if(lb != this->end() && *lb != v)
-          return this->end();
-        return lb;
+    template<typename... Ts>
+    constexpr auto emplace(Ts &&... vs) {
+      if constexpr(sizeof...(vs) == 1) {
+        container().emplace(this->lowerBound(vs...), flo::forward<Ts>(vs)...);
       }
+      else {
+        decay<decltype(container().front())> value{flo::forward<Ts>(vs)...};
+        container().emplace(this->lowerBound(value), flo::move(value));
+      }
+    }
 
-      template<typename Ty>
-      bool contains(Ty const &v) const {
-        return this->find(v) != this->end();
-      }
+    template<typename Ty>
+    constexpr auto insert(Ty const &v) {
+      return emplace(v);
+    }
 
-      template<typename Ty>
-      auto count(Ty const &v) const {
-        auto [b, e] = equalRange(v);
-        return std::distance(b, e);
-      }
+    template<typename Ty>
+    constexpr auto find(Ty const &v) const {
+      auto lb = this->lowerBound(v);
+      if(lb != container().end() && *lb != v)
+        return container().end();
+      return lb;
+    }
+
+    template<typename Ty>
+    constexpr bool contains(Ty const &v) const {
+      return this->find(v) != container().end();
+    }
+
+    template<typename Ty>
+    constexpr auto count(Ty const &v) const {
+      auto [b, e] = equalRange(v);
+      return flo::distance(b, e);
+    }
+
+  private:
+    constexpr auto &container()       { return static_cast<Storage       &>(*this); }
+    constexpr auto &container() const { return static_cast<Storage const &>(*this); }
   };
 }
