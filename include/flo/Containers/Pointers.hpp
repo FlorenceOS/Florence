1e3e7dd079ed2768ff1428a78b42cf9e2f51ac8c
N00byEdge <hannesbredberg@gmail.com>
2020-02-27 00:54:57 +0100

    Update Pointers.hpp

--- include/flo/Containers/Pointers.hpp
+++ include/flo/Containers/Pointers.hpp
@@ -2,17 +2,18 @@
 
 #include "Ints.hpp"
 
+#include "flo/Paging.hpp"
 #include "flo/Util.hpp"
 
 namespace flo {
   template<typename T>
-  struct OwnPtr {
+  union OwnPtr {
     OwnPtr() = default;
     // If one is destructed without deleting, hang
-    ~OwnPtr() { while(1); }
+    ~OwnPtr() { cleanup(); }
 
-    OwnPtr           (OwnPtr &&other) { ptr = flo::exchange(other.ptr, nullptr); }
-    OwnPtr &operator=(OwnPtr &&other) { ptr = flo::exchange(other.ptr, nullptr); }
+    OwnPtr           (OwnPtr &&other) {            ptr = flo::exchange(other.ptr, nullptr); }
+    OwnPtr &operator=(OwnPtr &&other) { cleanup(); ptr = flo::exchange(other.ptr, nullptr); }
 
     OwnPtr           (OwnPtr const &) = delete;
     OwnPtr &operator=(OwnPtr const &) = delete;
@@ -26,22 +27,27 @@ namespace flo {
     T *release() { return flo::exchange(ptr, nullptr); }
     T *get() const { return ptr; }
 
-    void reset(T *p = nullptr) { ptr = p; }
+    void reset(T *p = nullptr) { cleanup(); ptr = p; }
 
     T *operator->() const { return ptr; }
     T &operator* () const { return *ptr; }
+
+    void cleanup() {
+      if(ptr)
+        while(1){ }
+    }
   private:
     T *ptr;
   };
 
   template<typename T>
-  struct OwnPtr<T[]> {
+  union OwnPtr<T[]> {
     OwnPtr() = default;
     // If one is destructed without deleting, hang
-    ~OwnPtr() { while(1); }
+    ~OwnPtr() { cleanup(); }
 
-    OwnPtr           (OwnPtr &&other) { ptr = flo::exchange(other.ptr, nullptr); }
-    OwnPtr &operator=(OwnPtr &&other) { ptr = flo::exchange(other.ptr, nullptr); }
+    OwnPtr           (OwnPtr &&other) {            ptr = flo::exchange(other.ptr, nullptr); }
+    OwnPtr &operator=(OwnPtr &&other) { cleanup(); ptr = flo::exchange(other.ptr, nullptr); }
 
     OwnPtr           (OwnPtr const &) = delete;
     OwnPtr &operator=(OwnPtr const &) = delete;
@@ -55,12 +61,17 @@ namespace flo {
     T *release() { return flo::exchange(ptr, nullptr); }
     T *get() const { return ptr; }
 
-    void reset(T *p = nullptr) { ptr = p; }
+    void reset(T *p = nullptr) { cleanup(); ptr = p; }
 
     T *operator->() const { return ptr; }
     T &operator* () const { return *ptr; }
 
     T &operator[](uSz ind) const { return ptr[ind]; }
+
+    void cleanup() {
+      if(ptr)
+        while(1){ }
+    }
   private:
     T *ptr;
   };
