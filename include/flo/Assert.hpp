ca024f7d238241fa9c292c8d1c7856a9953d9ba2
N00byEdge <hannesbredberg@gmail.com>
2020-02-27 00:51:43 +0100

    Add asserts

---
+++ include/flo/Assert.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#define assert(cond)          do { if(!(cond)) flo::assertionFailure(__FILE__, __LINE__, #cond); } while(0)
+#define assert_err(cond, err) do { if(!(cond)) flo::assertionFailure(__FILE__, __LINE__, err); } while(0)
+#define assert_not_reached()  do { if(!(cond)) flo::assertionFailure(__FILE__, __LINE__, "Should be unreachable!"); } while(0)
+
+#define expect(cond)          do { if(!(cond)) flo::unexpected(__FILE__, __LINE__, #cond); } while(0)
+#define expect_err(cond, err) do { if(!(cond)) flo::unexpected(__FILE__, __LINE__, err); } while(0)
+#define expect_not_reached()  do { if(!(cond)) flo::unexpected(__FILE__, __LINE__, "Should be unreachable!"); } while(0)
+
+namespace flo {
+  void assertionFailure(char const *file, u64 line, char const *error);
+  void unexpected(char const *file, u64 line, char const *problem);
+}
