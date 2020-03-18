5543353308297b38875732dbced145c2ae46a579
N00byEdge <hannesbredberg@gmail.com>
2020-03-18 16:46:30 +0100

    Assert: Add missing Ints.hpp include

--- include/flo/Assert.hpp
+++ include/flo/Assert.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Ints.hpp"
+
 #define assert(cond)          do { if(!(cond)) flo::assertionFailure(__FILE__, __LINE__, #cond); } while(0)
 #define assert_err(cond, err) do { if(!(cond)) flo::assertionFailure(__FILE__, __LINE__, err); } while(0)
 #define assert_not_reached()  do {             flo::assertionFailure(__FILE__, __LINE__, "Should be unreachable!"); } while(0)
