e4de5bb2c6462b2ccfd6248558f7c11f121208f2
N00byEdge <hannesbredberg@gmail.com>
2020-03-30 22:12:52 +0200

    Bitfields: support for volatile

--- include/flo/Bitfields.hpp
+++ include/flo/Bitfields.hpp
@@ -2,12 +2,15 @@
 
 #include "Ints.hpp"
 
+#include "flo/TypeTraits.hpp"
+
 namespace flo {
   template<unsigned startBit_, unsigned numBits_, typename Container_ = u64>
   struct Bitfield {
   private:
+    using nvContainer = flo::removeCV<Container_>;
     constexpr static auto mask() {
-      Container out{};
+      nvContainer out{};
       for(unsigned i = startBit; i < startBit + numBits; ++i) {
         out |= Container{1} << i;
       }
@@ -15,10 +18,10 @@ namespace flo {
     }
 
   public:
-    constexpr Bitfield() = default;
-    constexpr Bitfield(Container_ val) { *this = val; }
-    
     using Container = Container_;
+
+    constexpr Bitfield() = default;
+    constexpr Bitfield(nvContainer val) { *this = val; }
 
     static constexpr auto startBit  = startBit_, numBits = numBits_;
     static constexpr auto selfMask  = mask();
@@ -26,25 +29,34 @@ namespace flo {
 
     static_assert(startBit + numBits <= sizeof(Container) * 8);
 
-    constexpr operator Container() const {
-      return (*reinterpret_cast<Container const *>(this) & selfMask) >> startBit;
+    constexpr operator nvContainer() const {
+      return (*reinterpret_cast<nvContainer const *>(this) & selfMask) >> startBit;
+    }
+
+    constexpr operator nvContainer() const volatile {
+      return (*reinterpret_cast<nvContainer const volatile *>(this) & selfMask) >> startBit;
     }
 
     constexpr auto operator=(Container val) {
-      return *reinterpret_cast<Container*>(this) =
-            (*reinterpret_cast<Container*>(this) & otherMask) | ((val << startBit) & selfMask);
+      return *reinterpret_cast<nvContainer *>(this) =
+            (*reinterpret_cast<nvContainer *>(this) & otherMask) | ((val << startBit) & selfMask);
     }
 
-    constexpr Bitfield &operator+= (Container_ val) { return *this = *this + val; }
-    constexpr Bitfield &operator-= (Container_ val) { return *this = *this - val; }
-    constexpr Bitfield &operator*= (Container_ val) { return *this = *this * val; }
-    constexpr Bitfield &operator/= (Container_ val) { return *this = *this / val; }
-    constexpr Bitfield &operator%= (Container_ val) { return *this = *this % val; }
-    constexpr Bitfield &operator^= (Container_ val) { return *this = *this ^ val; }
-    constexpr Bitfield &operator&= (Container_ val) { return *this = *this & val; }
-    constexpr Bitfield &operator|= (Container_ val) { return *this = *this | val; }
-    constexpr Bitfield &operator>>=(Container_ val) { return *this = *this >> val; }
-    constexpr Bitfield &operator<<=(Container_ val) { return *this = *this << val; }
+    constexpr auto operator=(Container val) volatile {
+      return *reinterpret_cast<nvContainer volatile *>(this) =
+            (*reinterpret_cast<nvContainer volatile *>(this) & otherMask) | ((val << startBit) & selfMask);
+    }
+
+    constexpr Bitfield &operator+= (nvContainer val) { return *this = *this + val; }
+    constexpr Bitfield &operator-= (nvContainer val) { return *this = *this - val; }
+    constexpr Bitfield &operator*= (nvContainer val) { return *this = *this * val; }
+    constexpr Bitfield &operator/= (nvContainer val) { return *this = *this / val; }
+    constexpr Bitfield &operator%= (nvContainer val) { return *this = *this % val; }
+    constexpr Bitfield &operator^= (nvContainer val) { return *this = *this ^ val; }
+    constexpr Bitfield &operator&= (nvContainer val) { return *this = *this & val; }
+    constexpr Bitfield &operator|= (nvContainer val) { return *this = *this | val; }
+    constexpr Bitfield &operator>>=(nvContainer val) { return *this = *this >> val; }
+    constexpr Bitfield &operator<<=(nvContainer val) { return *this = *this << val; }
     constexpr Bitfield &operator++ ()               { return *this = *this + 1; }
     constexpr Container operator++ (int)            { Container save = *this; ++(*this); return save; }
     constexpr Bitfield &operator-- ()               { return *this = *this - 1; }
