cfb464692111fbd96045c0595181b5899f9dcdb1
N00byEdge <hannesbredberg@gmail.com>
2021-01-15 01:06:56 +0100

    Mutex: Refactor, add held() and held_by_me()

--- src/thread/mutex.zig
+++ src/thread/mutex.zig
@@ -5,7 +5,7 @@ pub const Mutex = struct {
   queue: os.thread.WaitQueue = .{},
 
   fn lock_impl(self: *@This()) bool {
-    if(self.held_by) |h|
+    if(self.held())
       return false;
     self.held_by = os.platform.get_current_task();
     return true;
@@ -18,14 +18,21 @@ pub const Mutex = struct {
   }
 
   fn unlock_impl(self: *@This()) void {
-    @import("std").debug.assert(
-      self.held_by == os.platform.get_current_task()
-    );
-
+    @import("std").debug.assert(self.held_by_me());
     self.held_by = null;
   }
 
   pub fn unlock(self: *@This()) void {
     _ = self.queue.wake(unlock_impl, .{self});
+  }
+
+  pub fn held(self: *const @This()) bool {
+    if(self.held_by) |h|
+      return true;
+    return false;
+  }
+
+  pub fn held_by_me(self: *const @This()) bool {
+    return self.held_by == os.platform.get_current_task();
   }
 };
