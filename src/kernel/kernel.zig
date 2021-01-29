7aac84d0d3305f51eb99292b6bde98ae865b25c8
N00byEdge <hannesbredberg@gmail.com>
2021-01-29 23:43:45 +0100

    Remove accidental code

--- src/kernel/kernel.zig
+++ src/kernel/kernel.zig
@@ -5,12 +5,5 @@ const pci = os.platform.pci;
 pub fn kmain() noreturn {
   os.log("Hello, kmain!\n", .{});
 
-  var i: u64 = 0;
-  while(true) {
-    os.log("Hello, iteration {}\n", .{i});
-    i += 1;
-    os.thread.scheduler.yield();
-  }
-
   os.thread.scheduler.exit_task();
 }
