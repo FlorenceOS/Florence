36eff1ea466df9823014f2f26509bf20ad013063
Iurii Zamiatin <umz38@yandex.ru>
2021-02-03 14:09:36 +0300

    misc: Format src/lib

--- src/lib/debug.zig
+++ src/lib/debug.zig
@@ -17,76 +17,72 @@ extern var __debug_line_end: u8;
 extern var __debug_ranges_start: u8;
 extern var __debug_ranges_end: u8;
 
-var debug_info = std.dwarf.DwarfInfo {
-  .endian       = std.builtin.endian,
-  .debug_info   = undefined,
-  .debug_abbrev = undefined,
-  .debug_str    = undefined,
-  .debug_line   = undefined,
-  .debug_ranges = undefined,
+var debug_info = std.dwarf.DwarfInfo{
+    .endian = std.builtin.endian,
+    .debug_info = undefined,
+    .debug_abbrev = undefined,
+    .debug_str = undefined,
+    .debug_line = undefined,
+    .debug_ranges = undefined,
 };
 
 var inited_debug_info = false;
 
 fn init_debug_info() void {
-  if(!inited_debug_info) {
-    //debug_info.debug_info   = slice_section(&__debug_info_start, &__debug_info_end);
-    //debug_info.debug_abbrev = slice_section(&__debug_abbrev_start, &__debug_abbrev_end);
-    //debug_info.debug_str    = slice_section(&__debug_str_start, &__debug_str_end);
-    //debug_info.debug_line   = slice_section(&__debug_line_start, &__debug_line_end);
-    //debug_info.debug_ranges = slice_section(&__debug_ranges_start, &__debug_ranges_end);
-
-    os.log("Opening debug info\n", .{});
-    std.dwarf.openDwarfDebugInfo(&debug_info, debug_allocator) catch |err| {
-      os.log("Unable to open debug info: {}\n", .{@errorName(err)});
-      return;
-    };
-    os.log("Opened debug info\n", .{});
-    inited_debug_info = true;
-  }
+    if (!inited_debug_info) {
+        //debug_info.debug_info   = slice_section(&__debug_info_start, &__debug_info_end);
+        //debug_info.debug_abbrev = slice_section(&__debug_abbrev_start, &__debug_abbrev_end);
+        //debug_info.debug_str    = slice_section(&__debug_str_start, &__debug_str_end);
+        //debug_info.debug_line   = slice_section(&__debug_line_start, &__debug_line_end);
+        //debug_info.debug_ranges = slice_section(&__debug_ranges_start, &__debug_ranges_end);
+        os.log("Opening debug info\n", .{});
+        std.dwarf.openDwarfDebugInfo(&debug_info, debug_allocator) catch |err| {
+            os.log("Unable to open debug info: {}\n", .{@errorName(err)});
+            return;
+        };
+        os.log("Opened debug info\n", .{});
+        inited_debug_info = true;
+    }
 }
 
 pub fn dump_frame(bp: usize, ip: usize) void {
-  os.log("Dumping ip=0x{x}, bp=0x{x}\n", .{ip, bp});
-  init_debug_info();
+    os.log("Dumping ip=0x{x}, bp=0x{x}\n", .{ ip, bp });
+    init_debug_info();
 
-  print_addr(ip);
-
-  var it = std.debug.StackIterator.init(null, bp);
-  while(it.next()) |addr| {
     print_addr(ip);
-  }
+
+    var it = std.debug.StackIterator.init(null, bp);
+    while (it.next()) |addr| {
+        print_addr(ip);
+    }
 }
 
 pub fn dump_stack_trace(trace: *std.builtin.StackTrace) void {
-  init_debug_info();
+    init_debug_info();
 
-  for(trace.instruction_addresses[trace.index..]) |addr| {
-    print_addr(addr);
-  }
+    for (trace.instruction_addresses[trace.index..]) |addr| {
+        print_addr(addr);
+    }
 }
 
 fn slice_section(start: *u8, end: *u8) []const u8 {
-  const s = @ptrToInt(start);
-  const e = @ptrToInt(end);
-  return @intToPtr([*]const u8, s)[0..e-s];
+    const s = @ptrToInt(start);
+    const e = @ptrToInt(end);
+    return @intToPtr([*]const u8, s)[0 .. e - s];
 }
 
 fn print_addr(ip: usize) void {
-  var compile_unit = debug_info.findCompileUnit(ip) catch |err| {
-    os.log("Couldn't find the compile unit at {x}: {}\n", .{ip, @errorName(err)});
-    return;
-  };
+    var compile_unit = debug_info.findCompileUnit(ip) catch |err| {
+        os.log("Couldn't find the compile unit at {x}: {}\n", .{ ip, @errorName(err) });
+        return;
+    };
 
-  var line_info = debug_info.getLineNumberInfo(compile_unit.*, ip) catch |err| {
-    os.log("Couldn't find the line info at {x}: {}\n", .{ip, @errorName(err)});
-    return;
-  };
+    var line_info = debug_info.getLineNumberInfo(compile_unit.*, ip) catch |err| {
+        os.log("Couldn't find the line info at {x}: {}\n", .{ ip, @errorName(err) });
+        return;
+    };
 
-  print_info(line_info, ip, debug_info.getSymbolName(ip));
+    print_info(line_info, ip, debug_info.getSymbolName(ip));
 }
 
-fn print_info(line_info: std.debug.LineInfo, ip: usize, symbol_name: ?[]const u8) void {
-
-}
-
+fn print_info(line_info: std.debug.LineInfo, ip: usize, symbol_name: ?[]const u8) void {}
--- src/lib/panic.zig
+++ src/lib/panic.zig
@@ -3,24 +3,23 @@ const StackTrace = @import("builtin").StackTrace;
 const os = @import("root").os;
 
 pub fn breakpoint_panic(message: ?[]const u8, stack_trace: ?*StackTrace) noreturn {
-  @breakpoint();
-  unreachable;
+    @breakpoint();
+    unreachable;
 }
 
 pub fn panic(message: ?[]const u8, stack_trace: ?*StackTrace) noreturn {
-  if(message != null) {
-    os.log("PANIC: {}!\n", .{message});
-  }
-  else {
-    os.log("PANIC!!\n", .{});
-  }
+    if (message != null) {
+        os.log("PANIC: {}!\n", .{message});
+    } else {
+        os.log("PANIC!!\n", .{});
+    }
 
-  if(stack_trace) |trace| {
-    os.log("TODO: print stack trace.\nI bet this is very helpful. No problem.\n", .{});
-    os.lib.debug.dump_stack_trace(trace);
-  } else {
-    os.log("idfk I didn't get a stack trace.\n", .{});
-  }
+    if (stack_trace) |trace| {
+        os.log("TODO: print stack trace.\nI bet this is very helpful. No problem.\n", .{});
+        os.lib.debug.dump_stack_trace(trace);
+    } else {
+        os.log("idfk I didn't get a stack trace.\n", .{});
+    }
 
-  os.platform.hang();
+    os.platform.hang();
 }
