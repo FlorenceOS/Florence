fb8c5a84a9c3d99dd79cb24c410e9894780ba207
Iurii Zamiatin <umz38@yandex.ru>
2021-02-03 14:09:36 +0300

    misc: Format src/lib

--- src/lib/logger.zig
+++ src/lib/logger.zig
@@ -5,89 +5,89 @@ const arch = @import("builtin").arch;
 const range = os.lib.range.range;
 
 const Printer = struct {
-  pub fn writeAll(self: *const Printer, str: []const u8) !void {
-    try @call(.{.modifier = .never_inline}, print_str, .{str});
-  }
-
-  pub fn print(self: *const Printer, comptime format: []const u8, args: anytype) !void {
-    log_nolock(format, args);
-  }
-
-  pub fn writeByteNTimes(self: *const Printer, val: u8, num: usize) !void {
-    var i: usize = 0;
-    while(i < num): (i += 1) {
-      putch(val);
+    pub fn writeAll(self: *const Printer, str: []const u8) !void {
+        try @call(.{ .modifier = .never_inline }, print_str, .{str});
     }
-  }
 
-  pub const Error = anyerror;
+    pub fn print(self: *const Printer, comptime format: []const u8, args: anytype) !void {
+        log_nolock(format, args);
+    }
+
+    pub fn writeByteNTimes(self: *const Printer, val: u8, num: usize) !void {
+        var i: usize = 0;
+        while (i < num) : (i += 1) {
+            putch(val);
+        }
+    }
+
+    pub const Error = anyerror;
 };
 
 var log_lock: os.thread.Spinlock = .{};
 
 pub fn log(comptime format: []const u8, args: anytype) void {
-  const a = log_lock.lock();
-  defer log_lock.unlock(a);
+    const a = log_lock.lock();
+    defer log_lock.unlock(a);
 
-  return log_nolock(format, args);
+    return log_nolock(format, args);
 }
 
 fn log_nolock(comptime format: []const u8, args: anytype) void {
-  var printer = Printer{};
-  fmt.format(printer, format, args) catch unreachable;
+    var printer = Printer{};
+    fmt.format(printer, format, args) catch unreachable;
 }
 
 fn print_str(str: []const u8) !void {
-  for(str) |c| {
-    putch(c);
-  }
+    for (str) |c| {
+        putch(c);
+    }
 }
 
 fn putch(ch: u8) void {
-  os.platform.debugputch(ch);
-  os.drivers.mmio_serial.putch(ch);
-  os.drivers.vesa_log.putch(ch);
-  if(arch == .x86_64) {
-    os.drivers.vga_log.putch(ch);
-  }
+    os.platform.debugputch(ch);
+    os.drivers.mmio_serial.putch(ch);
+    os.drivers.vesa_log.putch(ch);
+    if (arch == .x86_64) {
+        os.drivers.vga_log.putch(ch);
+    }
 }
 
 pub fn hexdump_obj(val: anytype) void {
-  hexdump(@ptrCast([*]u8, val)[0..@sizeOf(@TypeOf(val.*))]);
+    hexdump(@ptrCast([*]u8, val)[0..@sizeOf(@TypeOf(val.*))]);
 }
 
 pub fn hexdump(in_bytes: []const u8) void {
-  var bytes = in_bytes;
-  while(bytes.len != 0) {
-    log("{X:0>16}: ", .{@ptrToInt(bytes.ptr)});
+    var bytes = in_bytes;
+    while (bytes.len != 0) {
+        log("{X:0>16}: ", .{@ptrToInt(bytes.ptr)});
 
-    inline for(range(0x10)) |offset| {
-      if(offset < bytes.len) {
-        const value = bytes[offset];
-        log("{X:0>2}{c}", .{value, if(offset == 7) '-' else ' '});
-      } else {
-        log("   ", .{});
-      }
-    }
-
-    inline for(range(0x10)) |offset| {
-      if(offset < bytes.len) {
-        const value = bytes[offset];
-        if(0x20 <= value and value < 0x7F) {
-          log("{c}", .{value});
-        } else {
-          log(".", .{});
+        inline for (range(0x10)) |offset| {
+            if (offset < bytes.len) {
+                const value = bytes[offset];
+                log("{X:0>2}{c}", .{ value, if (offset == 7) '-' else ' ' });
+            } else {
+                log("   ", .{});
+            }
         }
-      } else {
-        log(" ", .{});
-      }
+
+        inline for (range(0x10)) |offset| {
+            if (offset < bytes.len) {
+                const value = bytes[offset];
+                if (0x20 <= value and value < 0x7F) {
+                    log("{c}", .{value});
+                } else {
+                    log(".", .{});
+                }
+            } else {
+                log(" ", .{});
+            }
+        }
+
+        log("\n", .{});
+
+        if (bytes.len < 0x10)
+            return;
+
+        bytes = bytes[0x10..];
     }
-
-    log("\n", .{});
-
-    if(bytes.len < 0x10)
-      return;
-
-    bytes = bytes[0x10..];
-  }
 }
--- src/lib/tar.zig
+++ src/lib/tar.zig
@@ -3,51 +3,51 @@ const std = @import("std");
 const emptyarr: [0]u8 = undefined;
 
 const file_iterator = struct {
-  has_file: bool,
-  file_contents: []const u8,
-  file_name: []const u8,
-  remaining_blob: []const u8,
+    has_file: bool,
+    file_contents: []const u8,
+    file_name: []const u8,
+    remaining_blob: []const u8,
 
-  pub fn next(self: *@This()) void {
-    self.* = make_next(self.remaining_blob);
-  }
+    pub fn next(self: *@This()) void {
+        self.* = make_next(self.remaining_blob);
+    }
 };
 
 fn file_name(blob: []const u8) []const u8 {
-  // Filename is at offset 0, up to 100 in length
-  var len: usize = 0;
-  while(blob[len] != 0)
-    len += 1;
+    // Filename is at offset 0, up to 100 in length
+    var len: usize = 0;
+    while (blob[len] != 0)
+        len += 1;
 
-  return blob[0..len];
+    return blob[0..len];
 }
 
 fn file_size(blob: []const u8) usize {
-  // File size is at [124..135], octal encoded
-  return std.fmt.parseUnsigned(usize, blob[124..135], 8) catch unreachable;
+    // File size is at [124..135], octal encoded
+    return std.fmt.parseUnsigned(usize, blob[124..135], 8) catch unreachable;
 }
 
 fn make_next(blob: []const u8) file_iterator {
-  if(blob.len < 0x400 or std.mem.eql(u8, blob[0x101..0x106], &[_]u8{0} ** 5)) {
-    return file_iterator {
-      .has_file = false,
-      .file_contents = emptyarr[0..],
-      .file_name = emptyarr[0..],
-      .remaining_blob = emptyarr[0..],
+    if (blob.len < 0x400 or std.mem.eql(u8, blob[0x101..0x106], &[_]u8{0} ** 5)) {
+        return file_iterator{
+            .has_file = false,
+            .file_contents = emptyarr[0..],
+            .file_name = emptyarr[0..],
+            .remaining_blob = emptyarr[0..],
+        };
+    }
+
+    const size = file_size(blob);
+    const size_round_up = ((size + 0x1FF) / 0x200) * 0x200;
+
+    return file_iterator{
+        .has_file = true,
+        .file_contents = blob[0x200 .. 0x200 + size],
+        .file_name = file_name(blob),
+        .remaining_blob = blob[0x200 + size_round_up ..],
     };
-  }
-
-  const size = file_size(blob);
-  const size_round_up = ((size + 0x1FF) / 0x200) * 0x200;
-
-  return file_iterator {
-    .has_file = true,
-    .file_contents = blob[0x200..0x200 + size],
-    .file_name = file_name(blob),
-    .remaining_blob = blob[0x200 + size_round_up..],
-  };
 }
 
 pub fn iterate_files(tar_blob: []const u8) !file_iterator {
-  return make_next(tar_blob);
+    return make_next(tar_blob);
 }
