25f3fe68bb3add99cf4befb83316fcef1366f378
N00byEdge <hannesbredberg@gmail.com>
2021-02-06 11:46:03 +0100

    Panic: Print core ID, refactor a bit

--- src/lib/panic.zig
+++ src/lib/panic.zig
@@ -11,17 +11,17 @@ var panic_counter: usize = 0;
 /// You only panic once
 const YOPO = false;
 
-pub fn panic(message: ?[]const u8, stack_trace: ?*StackTrace) noreturn {
+pub fn panic(message_in: ?[]const u8, stack_trace: ?*StackTrace) noreturn {
     const panic_num = @atomicRmw(usize, &panic_counter, .Add, 1, .AcqRel) + 1;
 
     if(YOPO and panic_num != 1)
         os.platform.hang();
 
-    if (message) |m| {
-        os.log("PANIC {}: {}!\n", .{panic_num, m});
-    } else {
-        os.log("PANIC {}!!\n", .{panic_num});
-    }
+    const cpu = os.platform.get_current_cpu();
+    const cpu_id = cpu.id();
+    const message: []const u8 = message_in orelse "no message";
+
+    os.log("PANIC {}: CPU {}: {}!\n", .{panic_num, cpu_id, message});
 
     if (stack_trace) |trace| {
         os.log("TODO: print stack trace.\nI bet this is very helpful. No problem.\n", .{});
