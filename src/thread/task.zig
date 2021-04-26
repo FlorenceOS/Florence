f4731330a01e4f7bada7c484a5456251b4b37ecb
N00byEdge <hannesbredberg@gmail.com>
2021-04-26 12:41:52 +0200

    Writeback -> WriteBack typo

--- src/thread/task.zig
+++ src/thread/task.zig
@@ -37,7 +37,7 @@ pub const Task = struct {
       .virt = virt + guard_size,
       .size = map_size,
       .perm = os.memory.paging.rw(),
-      .memtype = .MemoryWriteback,
+      .memtype = .MemoryWriteBack,
     });
     self.stack = virt + total_size;
   }
