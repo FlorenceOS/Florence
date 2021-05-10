16498eb2d96bb2fb6812366ce43d5bcd9eef7802
N00byEdge <hannesbredberg@gmail.com>
2021-05-10 17:16:41 +0200

    Add some config.zig comments

--- src/config.zig
+++ src/config.zig
@@ -1,9 +1,18 @@
 pub const kernel = .{
+  // Max number of CPU cores to use
   .max_cpus = 0x200,
   
   .x86_64 = .{
+    // Allow using the `syscall` instruction to do syscalls
     .allow_syscall_instr = true,
+
+    // The maximum number of IOAPICs to use
     .max_ioapics = 5,
+
+    // Enable ps2 keyboard devices
+    // Your system managment mode may emulate a ps2 keyboard
+    // when you connect a usb keyboard. If you disable this,
+    // that won't work.
     .enable_ps2_keyboard = true,
   },
 };
