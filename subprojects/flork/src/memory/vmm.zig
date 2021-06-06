71445e3e7109300974588b060955fd602e5b7864
Iurii Zamiatin <umz38@yandex.ru>
2021-06-06 12:31:11 +0300

    misc: Move vmm to new coding style

--- subprojects/flork/src/memory/vmm.zig
+++ subprojects/flork/src/memory/vmm.zig
@@ -4,39 +4,6 @@ const paging = os.memory.paging;
 const RangeAlloc = os.memory.range_alloc.RangeAlloc;
 const Mutex = os.thread.Mutex;
 
-var sbrk_head: usize = undefined;
-
-pub fn init(phys_high: usize) !void {
-    os.log("Initializing vmm with base 0x{X}\n", .{phys_high});
-    sbrk_head = phys_high;
-}
-
-var sbrk_mutex = Mutex{};
-
-pub fn nonbacked_sbrk(num_bytes: usize) ![]u8 {
-    sbrk_mutex.lock();
-    defer sbrk_mutex.unlock();
-
-    const ret = sbrk_head;
-
-    sbrk_head += num_bytes;
-
-    return @intToPtr([*]u8, ret)[0..num_bytes];
-}
-
-pub fn sbrk(num_bytes: usize) ![]u8 {
-    const ret = try nonbacked_sbrk(num_bytes);
-
-    try paging.map(.{
-        .virt = @ptrToInt(ret.ptr),
-        .size = num_bytes,
-        .perm = paging.rw(),
-        .memtype = .MemoryWriteBack,
-    });
-
-    return ret;
-}
-
 /// Describes the lifetime of the memory aquired from an allocator
 const Lifetime = enum {
     /// Ephemeral memory won't last for the entire uptime of the kernel,
@@ -48,11 +15,11 @@ const Lifetime = enum {
     Eternal,
 };
 
+var sbrk_head: usize = undefined;
+var sbrk_mutex = Mutex{};
+
 /// Range allocator for backed memory
 var range = RangeAlloc{ .materialize_bytes = sbrk };
-
-/// Range allocator for nonbacked memory
-pub var nonbacked_range = RangeAlloc{ .materialize_bytes = nonbacked_sbrk };
 
 var ephemeral_alloc =
     std.heap.GeneralPurposeAllocator(.{
@@ -61,6 +28,38 @@ var ephemeral_alloc =
 }){
     .backing_allocator = &range.allocator,
 };
+
+/// Range allocator for nonbacked memory
+pub var nonbacked_range = RangeAlloc{ .materialize_bytes = nonbackedSbrk };
+
+pub fn init(phys_high: usize) !void {
+    os.log("Initializing vmm with base 0x{X}\n", .{phys_high});
+    sbrk_head = phys_high;
+}
+
+pub fn nonbackedSbrk(num_bytes: usize) ![]u8 {
+    sbrk_mutex.lock();
+    defer sbrk_mutex.unlock();
+
+    const ret = sbrk_head;
+
+    sbrk_head += num_bytes;
+
+    return @intToPtr([*]u8, ret)[0..num_bytes];
+}
+
+pub fn sbrk(num_bytes: usize) ![]u8 {
+    const ret = try nonbackedSbrk(num_bytes);
+
+    try paging.map(.{
+        .virt = @ptrToInt(ret.ptr),
+        .size = num_bytes,
+        .perm = paging.rw(),
+        .memtype = .MemoryWriteBack,
+    });
+
+    return ret;
+}
 
 /// The virtual memory is backed by physical pages.
 /// You can dereference these pointers just like in your
