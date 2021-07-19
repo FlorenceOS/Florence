08e62e4b87b7a5e9576fe525924c7e13b8658517
N00byEdge <hannesbredberg@gmail.com>
2021-07-19 14:31:48 +0200

    Libalign: add alignedCopy and alignedFill

--- lib/util/libalign.zig
+++ lib/util/libalign.zig
@@ -12,6 +12,32 @@ pub fn isAligned(comptime t: type, alignment: t, value: t) bool {
     return alignDown(t, alignment, value) == value;
 }
 
+fn sliceAlignCast(comptime t: type, slice: anytype) !(if (std.meta.trait.isConstPtr(@TypeOf(slice.ptr))) []const t else []t) {
+    const alignment = @alignOf(t);
+    comptime const ptr_type = if (std.meta.trait.isConstPtr(@TypeOf(slice.ptr))) [*]const t else [*]t;
+    if (lib.util.isRuntime() // we can't detect alignment of pointer values at comptime, not that it matters anyways
+    and isAligned(usize, alignment, @ptrToInt(slice.ptr)) // base ptr aligned
+    and isAligned(usize, alignment, slice.len)) { // length aligned
+        return @ptrCast(
+            ptr_type,
+            @alignCast(alignment, slice.ptr),
+        )[0..@divExact(slice.len, alignment)];
+    }
+    return error.NotAligned;
+}
+
+pub fn alignedCopy(comptime t: type, dest: []u8, src: []const u8) void {
+    const dest_aligned = sliceAlignCast(t, dest) catch return std.mem.copy(u8, dest, src);
+    const src_aligned = sliceAlignCast(t, src) catch return std.mem.copy(u8, dest, src);
+    return std.mem.copy(t, dest_aligned, src_aligned);
+}
+
+pub fn alignedFill(comptime t: type, dest: []u8, value: u8) void {
+    const dest_aligned = sliceAlignCast(t, dest) catch return std.mem.set(u8, dest, value);
+    const big_value = @truncate(t, 0x0101010101010101) * @intCast(t, value);
+    return std.mem.set(t, dest_aligned, big_value);
+}
+
 test "alignDown" {
     std.testing.expect(alignDown(u64, 0x1000, 0x4242) == 0x4000);
     std.testing.expect(alignDown(u64, 0x1000, 0x1000) == 0x1000);
