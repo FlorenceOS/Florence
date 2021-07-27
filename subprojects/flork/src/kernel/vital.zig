1b97ff7fde77e90761a12950214a61574e11a43e
N00byEdge <hannesbredberg@gmail.com>
2021-07-27 21:44:56 +0200

    Provide error return traces with vital()

--- subprojects/flork/src/kernel/vital.zig
+++ subprojects/flork/src/kernel/vital.zig
@@ -3,6 +3,6 @@ usingnamespace @import("root").preamble;
 pub fn vital(val: anytype, context: []const u8) @TypeOf(val catch unreachable) {
     return val catch |err| {
         os.log("Fatal Error: {s}, while: {s}\n", .{ @errorName(err), context });
-        @panic("Fatal error");
+        std.builtin.panic("Fatal error", @errorReturnTrace());
     };
 }
