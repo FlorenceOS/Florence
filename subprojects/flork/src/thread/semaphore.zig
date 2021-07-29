586b9ec43d4ae60610515750801ed19263e86bbb
Iurii Zamiatin <umz38@yandex.ru>
2021-07-29 17:37:41 +0300

    thread: Make semaphores killable

--- subprojects/flork/src/thread/semaphore.zig
+++ subprojects/flork/src/thread/semaphore.zig
@@ -6,6 +6,8 @@ const WaitingNode = struct {
     count: usize = undefined,
     /// Task waiting to be waken up
     task: *os.thread.Task = undefined,
+    /// Is semaphore dead?
+    dead: bool = false,
     /// Queue hook
     queue_hook: lib.containers.queue.Node = undefined,
 };
@@ -20,17 +22,24 @@ pub const Semaphore = struct {
     spinlock: os.thread.Spinlock = .{},
     /// Number of resources available
     available: usize,
+    /// Is semaphore dead?
+    dead: bool = false,
 
     /// Create semaphore with N resources available
     pub fn init(count: usize) @This() {
         return .{ .available = count };
     }
 
-    /// Acquire `count` resources. Don't call from interrupt context!
-    pub fn acquire(self: *@This(), count: usize) void {
+    /// Try to acquire `count` resources. Don't call from interrupt context!
+    pub fn try_acquire(self: *@This(), count: usize) !void {
         const task = os.platform.get_current_task();
 
         const lock_state = self.spinlock.lock();
+        if (self.dead) {
+            self.spinlock.unlock(lock_state);
+            return error.SemaphoreDead;
+        }
+
         if (self.available >= count) {
             self.available -= count;
             self.spinlock.unlock(lock_state);
@@ -44,12 +53,22 @@ pub const Semaphore = struct {
         self.queue.enqueue(&waiting_token);
         self.spinlock.ungrab();
         os.thread.scheduler.wait();
+
+        if (waiting_token.dead) {
+            return error.SemaphoreDead;
+        }
+
         os.platform.set_interrupts(lock_state);
     }
 
     /// Release `count` resources
     pub fn release(self: *@This(), count: usize) void {
         const lock_state = self.spinlock.lock();
+        if (self.dead) {
+            self.spinlock.unlock(lock_state);
+            return;
+        }
+
         self.available += count;
         if (self.queue.front()) |next| {
             const resources_needed = next.count;
@@ -60,5 +79,21 @@ pub const Semaphore = struct {
             }
         }
         self.spinlock.unlock(lock_state);
+    }
+
+    /// Kill semaphore
+    pub fn kill(self: *@This()) void {
+        const lock_state = self.spinlock.lock();
+        self.dead = true;
+        self.spinlock.unlock(lock_state);
+        while (self.queue.dequeue()) |wait_node| {
+            wait_node.dead = true;
+            os.thread.scheduler.wake(wait_node.task);
+        }
+    }
+
+    // Acquire `count` resources or panic. Don't call from interrupt context!
+    pub fn acquire(self: *@This(), count: usize) void {
+        self.try_acquire(count) catch { unreachable; };
     }
 };
