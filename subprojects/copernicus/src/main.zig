72bdc6a3f888500ca573af133755d733a12e5df3
N00byEdge <hannesbredberg@gmail.com>
2022-01-08 14:10:05 +0100

    Copernicus: Use linux syscalls
    Since https://github.com/ziglang/zig/pull/9491 we can now just straight
    up do this

--- subprojects/copernicus/src/main.zig
+++ subprojects/copernicus/src/main.zig
@@ -1,68 +1,11 @@
 const std = @import("std");
+const lib = @import("lib");
 
 fn logStr(str: []const u8) void {
-    switch (@import("builtin").target.cpu.arch) {
-        .x86_64 => {
-            // zig fmt: off
-            asm volatile(
-                \\int $0x80
-                \\
-                :
-                : [fd] "{rdi}" (@as(u64, 2))
-                , [str] "{rsi}" (@ptrToInt(str.ptr))
-                , [len] "{rdx}" (str.len)
-                , [syscall_num] "{rax}" (@as(u64, 1))
-            );
-            // zig fmt: on
-        },
-        .aarch64 => {
-            // zig fmt: off
-            asm volatile(
-                \\svc #0
-                \\
-                :
-                : [fd] "{X0}" (@as(u64, 2))
-                , [str] "{X1}" (@ptrToInt(str.ptr))
-                , [len] "{X2}" (str.len)
-                , [syscall_num] "{X8}" (@as(u64, 64))
-            );
-            // zig fmt: on
-        },
-        else => @compileError("logStr not implemented"),
-    }
-}
-
-fn exit(exit_code: u64) noreturn {
-    switch (@import("builtin").target.cpu.arch) {
-        .x86_64 => {
-            // zig fmt: off
-            asm volatile(
-                \\int $0x80
-                \\
-                :
-                : [num] "{rdi}" (exit_code)
-                , [syscall_num] "{rax}" (@as(u64, 60))
-            );
-            // zig fmt: on
-            unreachable;
-        },
-        .aarch64 => {
-            // zig fmt: off
-            asm volatile(
-                \\svc #0
-                \\
-                :
-                : [num] "{X0}" (exit_code)
-                , [syscall_num] "{X8}" (@as(u64, 93))
-            );
-            // zig fmt: on
-            unreachable;
-        },
-        else => @compileError("exit not implemented"),
-    }
+    _ = std.os.linux.write(2, str.ptr, str.len);
 }
 
 export fn _start() linksection(".text.entry") void {
     logStr("Hello, userspace!\n");
-    exit(0);
+    std.os.linux.exit(0);
 }
