f6044db788d35d43d66c1d7e58ef1e3c79f10d6f
N00byEdge <hannesbredberg@gmail.com>
2022-01-23 21:43:52 +0100

    virtio_pci: Fix allocation size

--- subprojects/flork/src/drivers/misc/virtio_pci.zig
+++ subprojects/flork/src/drivers/misc/virtio_pci.zig
@@ -176,13 +176,13 @@ pub const Driver = struct {
     /// Set up a specific queue
     fn setupQueue(drv: *Driver, i: u16) void {
         drv.cfg.queue_select = i;
-        const size = drv.cfg.queue_size;
-        if (size == 0) return;
-        const desc_siz: u32 = @sizeOf(VirtqDesc) * size;
-        const avail_siz: u32 = @sizeOf(VirtqAvail) + 2 + 2 * size;
-        const used_siz: u32 = @sizeOf(VirtqUsed) + 2 + @sizeOf(VirtqUsedItem) * size;
+        const queue_size = drv.cfg.queue_size;
+        if (queue_size == 0) return;
+        const desc_siz: u32 = @sizeOf(VirtqDesc) * queue_size;
+        const avail_siz: u32 = @sizeOf(VirtqAvail) + 2 + 2 * queue_size;
+        const used_siz: u32 = @sizeOf(VirtqUsed) + 2 + @sizeOf(VirtqUsedItem) * queue_size;
         const total_siz = desc_siz + avail_siz + used_siz;
-        const phys = os.memory.pmm.allocPhys(size) catch unreachable;
+        const phys = os.memory.pmm.allocPhys(total_siz) catch unreachable;
         const virt = os.platform.phys_ptr([*]volatile u8).from_int(phys).get_uncached();
         @memset(virt, 0x00, total_siz);
 
@@ -190,15 +190,15 @@ pub const Driver = struct {
             .desc = @ptrCast([*]VirtqDesc, virt),
             .avail = @ptrCast(*VirtqAvail, virt + desc_siz),
             .used = @ptrCast(*VirtqUsed, virt + desc_siz + avail_siz),
-            .size = size,
-            .num_unused = size,
+            .size = queue_size,
+            .num_unused = queue_size,
             .first_unused = 0,
             .last_in_used = 0,
             .pending = 0,
         };
 
         var m: u16 = 0;
-        while (m < size - 1) : (m += 1) {
+        while (m < queue_size - 1) : (m += 1) {
             drv.queues[i].desc[m] = .{
                 .flags = vring_desc_flag_next,
                 .next = m + 1,
